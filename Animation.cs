db250c99c8c5b9bfed67b5454cd9881efa07fa83
Matt Ball <mattdrivedev@gmail.com>
2023-10-12 23:24:18 +0100

    Animating the dino(s)

---
+++ Animation.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace IsoBlockEditor
+{
+    public class Animation
+    {
+        Texture2D _spritesheet;
+        Rectangle[] _frames;
+        int _currentFrame;
+        float _frameTime;
+
+        public Animation(Texture2D spritesheet, Rectangle[] frames)
+        {
+            _spritesheet = spritesheet;
+            _frames = frames;
+            _currentFrame = 0;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            _frameTime += (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (_frameTime > 0.1f)
+            {
+                _currentFrame++;
+                if (_currentFrame >= _frames.Length)
+                {
+                    _currentFrame = 0;
+                }
+                _frameTime = 0;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Rectangle destination)
+        {
+            spriteBatch.Draw(_spritesheet, destination, _frames[_currentFrame], Color.White);
+        }
+    }
+}
--- Dinos.cs
+++ Dinos.cs
@@ -6,20 +6,60 @@ namespace IsoBlockEditor
 {
     public abstract class Dino
     {
+        const int FRAME_WIDTH = 24;
+        const int FRAME_HEIGHT = 24;
+        const int FIRST_FRAME_IDLE = 0;
+        const int FRAME_COUNT_IDLE = 4;
+        const int FIRST_FRAME_WALK = 4;
+        const int FRAME_COUNT_WALK = 6;
+        const int FIRST_FRAME_KICK = 10;
+        const int FRAME_COUNT_KICK = 3;
+        const int FIRST_FRAME_HURT = 13;
+        const int FRAME_COUNT_HURT = 4;
+        const int FIRST_FRAME_CROUCH = 17;
+        const int FRAME_COUNT_CROUCH = 1;
+        const int FIRST_FRAME_SNEAK = 18;
+        const int FRAME_COUNT_SNEAK = 6;
+
         Vector2 _position;
         Texture2D _spritesheet;
         Texture2D _shadow;
+        Animation _idle;
+        Animation _walk;
+        Animation _kick;
+        Animation _hurt;
+        Animation _crouch;
+        Animation _sneak;
+        Animation _currentAnimation;
 
         public Dino(ContentManager content, string spritesheet, Vector2 position)
-        {
+        {   
             _spritesheet = content.Load<Texture2D>(spritesheet);
             _shadow = content.Load<Texture2D>("dinos/shadow");
-            _position = position;
+            _position = position;                                 
+            _idle = new Animation(_spritesheet, CreateFrames(FIRST_FRAME_IDLE, FRAME_COUNT_IDLE));            
+            _walk = new Animation(_spritesheet, CreateFrames(FIRST_FRAME_WALK, FRAME_COUNT_WALK));
+            _kick = new Animation(_spritesheet, CreateFrames(FIRST_FRAME_KICK, FRAME_COUNT_KICK));
+            _hurt = new Animation(_spritesheet, CreateFrames(FIRST_FRAME_HURT, FRAME_COUNT_HURT));
+            _crouch = new Animation(_spritesheet, CreateFrames(FIRST_FRAME_CROUCH, FRAME_COUNT_CROUCH));
+            _sneak = new Animation(_spritesheet, CreateFrames(FIRST_FRAME_SNEAK, FRAME_COUNT_SNEAK));
+            _currentAnimation = _idle;
+        }
+
+        private Rectangle[] CreateFrames(int firstFrame, int frameCount)
+        {
+            var frames = new Rectangle[frameCount];
+            for (var i = 0; i < frameCount; i++)
+            {
+                var x = (firstFrame + i) * FRAME_WIDTH;
+                frames[i] = new Rectangle(x, 0, FRAME_WIDTH, FRAME_HEIGHT);
+            }
+            return frames;
         }
 
         public void Update(GameTime gameTime)
         {
-
+            _currentAnimation.Update(gameTime);
         }
 
         public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
@@ -34,15 +74,31 @@ namespace IsoBlockEditor
             var source = new Rectangle(0, 0, 24, 24);
 
             spriteBatch.Draw(_shadow, destination, source, Color.White);
-            spriteBatch.Draw(_spritesheet, destination, source, Color.White);
+            _currentAnimation.Draw(spriteBatch, destination);
         }
     }
 
     public class Red : Dino
     {
-        public Red(ContentManager content, Vector2 position) : base(content, "dinos/red", position)
-        {
+        public Red(ContentManager content, Vector2 position) 
+            : base(content, "dinos/red", position) { }
+    }
 
-        }
+    public class Blue : Dino
+    {
+        public Blue(ContentManager content, Vector2 position) 
+            : base(content, "dinos/blue", position) { }
+    }
+
+    public class Green : Dino
+    {
+        public Green(ContentManager content, Vector2 position) 
+            : base(content, "dinos/green", position) { }
+    }
+
+    public class Yellow : Dino
+    {
+        public Yellow(ContentManager content, Vector2 position) 
+            : base(content, "dinos/yellow", position) { }
     }
 }
